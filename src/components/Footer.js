c13c6733a7fe22bfab7cb4d731a635c6a88b1198
raavikant27 <sravikant302@gmail.com>
2023-12-27 16:56:49 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>
+        Copyright
+        <CopyrightIcon /> {year}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
